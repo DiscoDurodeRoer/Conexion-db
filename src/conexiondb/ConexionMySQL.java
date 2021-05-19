dc72fca7f2a8a21f0bedd6fb6d7f3406ebe6262b
elmachado <elmachado@hotmail.es>
2021-05-19 00:10:33 -0500

    Se actualiza la versión del driver a la más reciente, remplazando com.mysql.jdbc.Driver por com.mysql.cj.jdbc.Driver.

--- src/conexiondb/ConexionMySQL.java
+++ src/conexiondb/ConexionMySQL.java
@@ -2,12 +2,13 @@ package conexiondb;
 
 public class ConexionMySQL extends ConexionDB {
 
+
     public ConexionMySQL(String host, String puerto, String baseDatos, String usuario, String password) {
-        super("com.mysql.jdbc.Driver", "jdbc:mysql://" + host + ":" + puerto + "/" + baseDatos, usuario, password);
+        super("com.mysql.cj.jdbc.Driver", "jdbc:mysql://" + host + ":" + puerto + "/" + baseDatos, usuario, password);
     }
 
     public ConexionMySQL(String host, String baseDatos, String usuario, String password) {
-        super("com.mysql.jdbc.Driver", "jdbc:mysql://" + host + "/" + baseDatos, usuario, password);
+        super("com.mysql.cj.jdbc.Driver", "jdbc:mysql://" + host + "/" + baseDatos, usuario, password);
     }
 
 }
